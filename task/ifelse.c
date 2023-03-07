d12dd14e8da6e45c9048ceb03f0add3789bf407b
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-07 12:15:21 +0530

    Add files via upload

---
+++ task/ifelse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a, b;
+    printf("Enter the value of a and :");
+    scanf("%d%d",&a,&b);
+    if(a,b)
+    {
+        printf("both are equal");
+    }
+    else
+    {
+        printf("both are not equal");
+    }
+}
