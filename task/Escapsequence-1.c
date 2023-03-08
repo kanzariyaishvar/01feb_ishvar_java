e66c94da2276dfacef80dc3095eaf10a1b790835
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-09 05:19:59 +0530

    Add files via upload

---
+++ task/Escapsequence-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+    void main()
+
+    {
+    printf(" \tIndian NationalCricketer  Name-\n\t'Indian Cricketer'");
+    printf(" Captain-\n\tVirat Kohli");
+    printf("\n\n\tCricketer Details Chart"); 
+    printf("\n\nNo              Name          Over            Four              Six"); 
+    printf("\n1)             Virat Kohli       10              6                4");  
+    printf("\n2)             Rohit Sharma      10              5                6");  
+    printf("\n3)             Kl Rahul 10       10              8                7");  
+    printf("\n4)             Hardik Pandiya    10              6                9");  
+    printf("\n5)             Rishabh Pant      10              3                15");  
+    printf("\n6)             Ravindra Jadeja   10              6                0");
+    printf("\n7)             Ms Dhoni    10    10              7                6");  
+    printf("\n8)             Jasprit Bumrah    10              9                8");  
+    printf("\n9)             Yuzvendra Chahal  10              4                10");
+    printf("\n10)            Kuldeep Yadav     10              5                10");
+    printf("\n11)            Mayank Agraval    10              6                10");
+    printf("\n\n\nThanks All You");
+    }
