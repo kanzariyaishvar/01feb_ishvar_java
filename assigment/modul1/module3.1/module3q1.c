d414a795187985c3e5411737a8f24cbb3c1975ba
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-09 11:42:09 +0530

    Delete module3q1.c

--- assigment/modul1/module3.1/module3q1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("\n\t1Name:- Kanzariya Ishvar PranjivanBhai");
-    printf("\n\t2Birth Date:- 12/8/2003");
-    printf("\n\t3Age:- 19 YEar");
-    printf("\n\t\t4Adress:- Navlakhi Road \n\t\tVillage Gorkhijadiya \n\t\tMasalvadi \n\t\tMorbi Gujrat-363641");
-}
