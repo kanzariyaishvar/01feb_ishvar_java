c15c53d62189f91d4ade5695216ee816443aa181
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-10 09:38:00 +0530

    Add files via upload

---
+++ task/Name.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//Globle variable
+char nm;
+void main()
+{
+    printf("Enter your Name is First's latter:");
+    scanf("%c",&nm);
+    printf("\nYour name is first latter is:%c",nm);
+}
