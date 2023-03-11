68a8c05418d12ff5bbabca8cdd48ae303728253f
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-11 10:45:57 +0530

    Delete displayinformation.c

--- assigment/module-1/displayinformation.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    printf("My Name is:        Kanzariya ishvar");
-    printf("\n\nBirth date is:     12/8/2003");
-    printf("\n\nMy Age:            19 year");
-    printf("\nAddress is :       Navlakhi Road ,Village gorkhijadiya,");
-    printf("\n                   Masal Vadi,Morbi-363641.");
-}
