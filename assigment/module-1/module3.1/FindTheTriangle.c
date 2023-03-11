3174f361eb0450862840e5078a87c26d476153af
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-11 12:36:17 +0530

    Add files via upload

---
+++ assigment/module-1/module3.1/FindTheTriangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float height;
+    float base;
+    float area;
+    printf("Enter the base:-");
+    scanf("%f",&height);
+    printf("Enter the Height:-");
+    scanf("%f",&base);
+    area= height*base/2;
+    printf("Totle Triangle Value Is:-%f",area);
+}
