7e647b5e8bd66cd8ba22b67b58bf27b681bc0173
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-11 10:44:06 +0530

    Add files via upload

---
+++ assigment/module-1/displayinformation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    printf("My Name is:        Kanzariya ishvar");
+    printf("\n\nBirth date is:     12/8/2003");
+    printf("\n\nMy Age:            19 year");
+    printf("\nAddress is :       Navlakhi Road ,Village gorkhijadiya,");
+    printf("\n                   Masal Vadi,Morbi-363641.");
+}
