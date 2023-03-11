00a0c4f413113b177a786764c16c7a18e79f8f1c
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-11 12:12:54 +0530

    Add files via upload

---
+++ assigment/module-1/module3.1/FindTheRectengle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    float width;
+    float lenth;
+    float area;
+    printf("Enter The Value Rectengle:-");
+    scanf("%f",&lenth);
+    printf("Enter The Value Rectengle:-");
+    scanf("%f",&width);
+    area = lenth*width;
+    printf("Totale Value Is Rectengle:-%f",area);
+}
