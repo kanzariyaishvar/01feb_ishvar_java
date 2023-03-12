4c80bd9fc4cb853317de7f49ff9b4bead19fde61
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-12 23:20:05 +0530

    Add files via upload

---
+++ assigment/module-1/module3.1/daytoyear.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float year;
+    float days;
+    printf("Eter year:");
+    scanf("%f",&year);
+    days = year*365;
+    printf("year to iqual day%f",days);
+
+}
