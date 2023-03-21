da5d2eb1c6b9743e892afdfbc299b3cdd6ae94de
kanzariyaishvar <124335698+kanzariyaishvar@users.noreply.github.com>
2023-03-21 12:40:35 +0530

    Add files via upload

---
+++ assigment/module3.2/piramid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    for (i=5 ; i > 0; i--)
+    {
+        for ( j =;j <i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
